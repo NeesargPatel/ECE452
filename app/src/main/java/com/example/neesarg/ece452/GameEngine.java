5cf4c7c9dc9fb63f28204caaa0a24391bd46d2cb
Neesarg Patel <n94patel@uwaterloo.ca>
2018-02-25 18:47:54 -0500

    Initial commit

---
+++ app/src/main/java/com/example/neesarg/ece452/GameEngine.java
@@ -0,0 +1,15 @@
+package com.example.neesarg.ece452;
+
+/**
+ * Created by Neesarg on 2/24/2018.
+ */
+
+public class GameEngine {
+    public GameEngine() {
+
+    }
+
+    public boolean isEnded() {
+        return false;
+    }
+}
